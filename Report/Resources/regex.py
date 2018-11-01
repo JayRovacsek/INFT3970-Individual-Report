15d075658ee3ee6b3526b3ac0ef0864017a156a8
Jay Rovacsek <jayrovacsek@gmail.com>
2018-11-01 19:25:49 +1100

    Update python redos

--- Report/Resources/regex.py
+++ Report/Resources/regex.py
@@ -6,6 +6,7 @@ import time
 if __name__ == '__main__':
     
     email = 'c3146220@uon.edu.au'
+    attack_email = '(c)(3)(1)(4)(6220)@uon.edu.au'
     simple_pattern = r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b'
     complex_pattern = r'^(?=[A-Z0-9._%+-]{6,254}$)[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$'
 
@@ -15,6 +16,16 @@ if __name__ == '__main__':
     print(computation_time)
 
     start_time = time.time()
+    re.match(simple_pattern, attack_email, flags=0)
+    computation_time = time.time() - start_time
+    print(computation_time)
+
+    start_time = time.time()
     re.match(complex_pattern, email, flags=0)
+    computation_time = time.time() - start_time
+    print(computation_time)
+
+    start_time = time.time()
+    re.match(complex_pattern, attack_email, flags=0)
     computation_time = time.time() - start_time
     print(computation_time)
